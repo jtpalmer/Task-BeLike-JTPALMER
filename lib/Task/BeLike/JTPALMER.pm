dbdd69571c82ffa2ca611adf99c23daf623cfcb8
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-04-23 19:09:09 -0400

    Start adding modules

--- lib/Task/BeLike/JTPALMER.pm
+++ lib/Task/BeLike/JTPALMER.pm
@@ -2,5 +2,129 @@ package Task::BeLike::JTPALMER;
 use strict;
 use warnings;
 
+# ABSTRACT: Modules used by JTPALMER
+
 1;
+
+__END__
+
+=pkgroup CPAN Shell
+
+=pkg Term::ReadLine::Perl
+
+=pkgroup Object/Class Modules
+
+=pkg parent
+
+=pkg Moose
+
+=pkg MooseX::Types
+
+=pkg Mouse
+
+=pkg Any::Moose
+
+=pkg namespace::clean
+
+=pkg namespace::autoclean
+
+=pkgroup Toolchain
+
+=pkg Dist::Zilla
+
+=pkg Alien::Base
+
+=pkg File::ShareDir
+
+=pkgroup Applications
+
+=pkg App::perlbrew
+
+=pkg App::cpanminus
+
+=pkg App::Ack
+
+=pkg App::Nopaste
+
+=pkg Perl::Tidy
+
+=pkg Perl::Critic
+
+=pkgroup Module Installation
+
+=pkg local::lib
+
+=pkgroup Configuration File Parses
+
+=pkg Config::Tiny
+
+=pkg YAML::Tiny
+
+=pkg JSON::XS
+
+=pkgroup Email
+
+=pkg Email::Sender
+
+=pkg Email::Simple
+
+=pkg Email::Valid
+
+=pkg Email::MIME
+
+=pkg Email::MIME::Kit
+
+=pkgroup Utilities
+
+=pkg List::MoreUtils
+
+=pkg Capture::Tiny
+
+=pkg URI
+
+=pkg File::Slurp
+
+=pkg DateTime
+
+=pkg Regexp::Common
+
+=pkg IO::All
+
+=pkgroup Templating
+
+=pkg Template
+
+=pkgroup Web Development
+
+=pkg PSGI
+
+=pkg Plack
+
+=pkg Starman
+
+=pkg Catalyst
+
+=pkg Catalyst::Devel
+
+=pkg Catalyst::Engine::PSGI
+
+=pkgroup Command Line Development
+
+=pkg MooseX::App::Cmd
+
+=pkgroup Database Support
+
+=pkg DBI
+
+=pkg DBD::SQLite
+
+=pkg DBIx::Class
+
+=pkg BerkeleyDB
+
+=pkgroup Financial Modules
+
+=pkg Finance::Quote
+
+=cut
 
